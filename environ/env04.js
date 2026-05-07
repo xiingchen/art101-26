6b4f5dff0f5fc7fd59dd3cc43fd383549f9af5a8
xiingchen <boverylong@gmail.com>
2026-05-06 23:23:23 -0700

    lab 5 env 05

---
+++ environ/env04.js
@@ -0,0 +1,83 @@
+$(function () {
+
+    let currentIndex = 0;
+
+    let lyricSets = [
+        [
+            "I feel like everybody's singing out of tune",
+            "I feel like I can't help but always be so blue",
+            "But in the end, I know I must keep pulling through",
+            "And brace myself for all the hell-like petals on the moon"
+        ],
+        [
+            "How the hell did I fall in love this time?",
+            "And honestly, I cant believe I get to call you mine",
+            "I blinked and suddenly",
+            "I had a Valentine"
+        ],
+        [
+            "We could have been so good together",
+            "We could have lived this dance forever",
+            "But now, who's gonna dance with me?",
+            "Please stay"
+        ]
+    ];
+
+    function showLyrics(index) {
+        let lyricsHTML = "";
+
+        for (let i = 0; i < lyricSets[index].length; i++) {
+            lyricsHTML += "<p>" + lyricSets[index][i] + "</p>";
+        }
+
+        $(".lyrics").html(lyricsHTML);
+    }
+
+    showLyrics(currentIndex);
+
+    $("#forward-button").click(function () {
+        currentIndex++;
+
+        if (currentIndex >= lyricSets.length) {
+            currentIndex = 0;
+        }
+
+        showLyrics(currentIndex);
+    });
+
+    $("#back-button").click(function () {
+        currentIndex--;
+
+        if (currentIndex < 0) {
+            currentIndex = lyricSets.length - 1;
+        }
+
+        showLyrics(currentIndex);
+    });
+
+    $("#play-button").click(function () {
+        let img = $("#play-button");
+
+        if (img.attr("src").includes("play.png")) {
+            img.attr("src", "../exercises/images/pause.png");
+        } else {
+            img.attr("src", "../exercises/images/play.png");
+        }
+    });
+
+function askWhat(correctWord) {
+    let userWord = prompt("Type the secret word:");
+
+    if (userWord.toLowerCase() == correctWord) {
+        $("#output").html("Chicken butt!");
+    }
+    else {
+        $("#output").html("Say what?");
+    }
+}
+
+$("#what-button").click(function () {
+    askWhat("what");
+});
+
+});
